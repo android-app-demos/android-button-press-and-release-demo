787d2ad911794fc8eb3adad4ef1bfcd5c8639ac1
freewind <nowindlee@gmail.com>
2018-08-08 00:43:48 +0800

    demo

--- app/src/main/java/demos/MainActivity.kt
+++ app/src/main/java/demos/MainActivity.kt
@@ -1,13 +1,25 @@
 package demos
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val button by lazy<Button> { findViewById(R.id.button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnTouchListener { _, event ->
+            when (event!!.action) {
+                MotionEvent.ACTION_DOWN -> button.text = "Pressing"
+                MotionEvent.ACTION_UP -> button.text = "Released"
+            }
+            true
+        }
     }
 
 }
